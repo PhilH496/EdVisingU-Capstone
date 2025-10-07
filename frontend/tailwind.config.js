7bbe10586cb9f5ea0e290ce008f21566a39da3e6
Nathan Le <lenathanjon@gmail.com>
2025-10-07 15:46:08 -0700

    style: apply brand colors to form inputs and navigation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,13 +9,13 @@ module.exports = {
     extend: {
       colors: {
         'brand-dark-blue': '#0071a9',
-        'brand-light-blue': '#0d6efd',
+        'brand-light-blue': '#d1ddeb',
         'brand-light-green': '#e6fad2',
         'brand-light-yellow': '#FAFAD2',
-        'brand-light-red': '#fad2d2',
-        'brand-submit-green': '#90EE90',
+        'brand-light-red': '#d62929',
+        'brand-green': '#e6fad2',
         'brand-text-gray': '#4e4e4e',
-        'brand-header-black': '#333',
+        'brand-black': '#333333',
       },
       fontFamily: {
         'raleway': ['Raleway', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
