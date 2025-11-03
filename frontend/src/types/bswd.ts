2d6d985a67da49f399ab987cde1f1c729648cc71
aidanbeardslee <aidanbeardslee@gmail.com>
2025-11-02 19:22:47 -0700

    fix: change disabilityType requirements

--- frontend/src/types/bswd.ts
+++ frontend/src/types/bswd.ts
@@ -42,7 +42,7 @@ export interface FormData {
 
   // Disability Information
   hasVerifiedDisability: boolean;
-  disabilityType?: "permanent" | "persistent-prolonged" | "not-verified" | undefined;
+  disabilityType: 'permanent' | 'persistent-prolonged' | 'not-verified';
   disabilityVerificationDate: string;
   functionalLimitations: string[];
   needsPsychoEdAssessment: boolean;
